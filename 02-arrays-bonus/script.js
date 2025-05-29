986f78a94fc164daa0da2ad022f0aa875b563803
simone-scotto <simone.scottotkd@gmail.com>
2025-05-29 17:56:40 +0200

    half bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,15 +8,41 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
+
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = []
+
+for ( let i = 0; i < teachers.length; i++) {
+  let element = teachers[i];
+  if (element.length >= 5) {
+
+
+    longNames.push(element)   
+    
+
+  }
+
+} console.log(longNames);
+
+
+
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+
+teachers.splice (teachers.indexOf("Ed"), 1);
+
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
